fe622ef94aecbbb1bcf2a1cc9fd29976c75776b6
Dogthemachine <danileyko@gmail.com>
2022-05-15 14:40:19 +0300

    finishing navbar

--- human_hub/human_hub/settings.py
+++ human_hub/human_hub/settings.py
@@ -116,13 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = env('HU_MEDIA_ROOT')
 
-STATIC_ROOT = env('HU_STATIC_ROOT')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
