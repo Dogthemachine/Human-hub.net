14616db5fd5a54400b845f3147fc9afcfa5999e8
Dogthemachine <danileyko@gmail.com>
2022-05-16 20:29:32 +0300

    makking footer

--- human_hub/human_hub/settings.py
+++ human_hub/human_hub/settings.py
@@ -72,7 +72,6 @@ WSGI_APPLICATION = 'human_hub.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -83,7 +82,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -102,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -114,8 +111,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 
 STATIC_ROOT = env('HU_STATIC_ROOT')
 
@@ -127,7 +122,6 @@ MEDIA_ROOT = env('HU_MEDIA_ROOT')
 
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
