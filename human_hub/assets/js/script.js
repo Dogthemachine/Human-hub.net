c736eedf6adb763dc1c80e45776f43c57a16af7f
Dogthemachine <danileyko@gmail.com>
2022-06-04 16:28:37 +0300

    fixing make order modal window

--- human_hub/assets/js/script.js
+++ human_hub/assets/js/script.js
@@ -13,19 +13,84 @@ $(document).ready(function() {
     });
 
 
-    const makeorder = document.getElementById("pop-order")
-    makeorder.addEventListener('click', function(event) {
+    const makeorder = document.getElementById("pop-order");
+    makeorder.addEventListener('click', showOrderModal);
+
+    function showOrderModal() {
         $.ajax({
             url: '/orders/order',
             type: 'get',
-            success: function(data) {
-                $('#hb-order-content').html(data.html);
-            }
+            success: makeorderSuccess
         });
-    });
+    };
+
+    function makeorderSuccess(data) {
+        $('#hb-order-content').html(data.html);
+        const worldwide = document.getElementById("ordr-deliv-worldwide");
+        const novpostobranch = document.getElementById("ordr-deliv-novpostobranch");
+        const novpostodoor = document.getElementById("ordr-deliv-novpostodoor");
+        const urkpostobranch = document.getElementById("ordr-deliv-urkpostobranch");
+        const urkpostodoor = document.getElementById("ordr-deliv-urkpostodoor");
+        const justintobranch = document.getElementById("ordr-deliv-justintobranch");
+
+        worldwide.addEventListener('click', () => {
+            if (worldwide.checked) {
+                novpostobranch.checked = false
+                novpostodoor.checked = false
+                urkpostobranch.checked = false
+                urkpostodoor.checked = false
+                justintobranch.checked = false
+            };
+        });
+        novpostobranch.addEventListener('click', () => {
+            if (novpostobranch.checked) {
+                worldwide.checked = false
+                novpostodoor.checked = false
+                urkpostobranch.checked = false
+                urkpostodoor.checked = false
+                justintobranch.checked = false
+            };
+        });
+        novpostodoor.addEventListener('click', () => {
+            if (novpostodoor.checked) {
+                novpostobranch.checked = false
+                worldwide.checked = false
+                urkpostobranch.checked = false
+                urkpostodoor.checked = false
+                justintobranch.checked = false
+            };
+        });
+        urkpostobranch.addEventListener('click', () => {
+            if (urkpostobranch.checked) {
+                novpostobranch.checked = false
+                novpostodoor.checked = false
+                worldwide.checked = false
+                urkpostodoor.checked = false
+                justintobranch.checked = false
+            };
+        });
+        urkpostodoor.addEventListener('click', () => {
+            if (urkpostodoor.checked) {
+                novpostobranch.checked = false
+                novpostodoor.checked = false
+                urkpostobranch.checked = false
+                worldwide.checked = false
+                justintobranch.checked = false
+            };
+        });
+        justintobranch.addEventListener('click', () => {
+            if (justintobranch.checked) {
+                novpostobranch.checked = false
+                novpostodoor.checked = false
+                urkpostobranch.checked = false
+                urkpostodoor.checked = false
+                worldwide.checked = false
+            };
+        });
+    };
 
 
-    const toggle = document.querySelector('.toggle input')
+    const toggle = document.querySelector('.toggle input');
     toggle.addEventListener('click', () => {
         const onOff = toggle.parentNode.querySelector('.onoff');
         document.getElementById('icontoggler').src = toggle.checked ? "/static/img/category_toggle.png" : "/static/img/category_toggle_checked.png";
