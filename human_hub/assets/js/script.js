2652e55d7cc7719f0a61d9cd8fcbf92acebb6c82
Dogthemachine <danileyko@gmail.com>
2022-06-06 16:02:54 +0300

    fixed make order switches

--- human_hub/assets/js/script.js
+++ human_hub/assets/js/script.js
@@ -30,6 +30,8 @@ $(document).ready(function() {
     };
 
 
+//-----------   MAKE ORDER MODAL WINDOW     -----------
+
     const makeorder = document.getElementById("show-order-modal");
     makeorder.addEventListener('click', showOrderModal);
 
@@ -51,63 +53,176 @@ $(document).ready(function() {
         const urkpostodoor = document.getElementById("ordr-deliv-urkpostodoor");
         const justintobranch = document.getElementById("ordr-deliv-justintobranch");
 
-        const collapsing = document.getElementById("collapseDeliveryInfo");
+        const WorldWide = document.getElementById("collapseDeliveryWorldWide");
+        const NovaPoshtaToBranch = document.getElementById("collapseDeliveryNovaPoshtaToBranch");
+        const NovaPoshtaToDoor = document.getElementById("collapseDeliveryNovaPoshtaToDoor");
+        const UkrposhtaToBranch = document.getElementById("collapseDeliveryUkrposhtaToBranch");
+        const UkrposhtaToDoor = document.getElementById("collapseDeliveryUkrposhtaToDoor");
+        const JustinToBranch = document.getElementById("collapseDeliveryJustinToBranch");
+
+        NovaPoshtaToBranch.style.display = 'none';
+        NovaPoshtaToDoor.style.display = 'none';
+        UkrposhtaToBranch.style.display = 'none';
+        JustinToBranch.style.display = 'none';
+        UkrposhtaToDoor.style.display = 'none';
+        WorldWide.style.display = 'none';
 
         worldwide.addEventListener('click', () => {
             if (worldwide.checked) {
-                novpostobranch.checked = false
-                novpostodoor.checked = false
-                urkpostobranch.checked = false
-                urkpostodoor.checked = false
-                justintobranch.checked = false
+                NovaPoshtaToBranch.style.display = 'none';
+                NovaPoshtaToDoor.style.display = 'none';
+                UkrposhtaToBranch.style.display = 'none';
+                JustinToBranch.style.display = 'none';
+                UkrposhtaToDoor.style.display = 'none';
+                WorldWide.style.display = 'block';
+                novpostobranch.checked = false;
+                novpostodoor.checked = false;
+                urkpostobranch.checked = false;
+                urkpostodoor.checked = false;
+                justintobranch.checked = false;
+                } else {
+            WorldWide.style.display = 'none';
             };
         });
         novpostobranch.addEventListener('click', () => {
             if (novpostobranch.checked) {
-                worldwide.checked = false
-                novpostodoor.checked = false
-                urkpostobranch.checked = false
-                urkpostodoor.checked = false
-                justintobranch.checked = false
+                WorldWide.style.display = 'none';
+                NovaPoshtaToDoor.style.display = 'none';
+                UkrposhtaToBranch.style.display = 'none';
+                JustinToBranch.style.display = 'none';
+                UkrposhtaToDoor.style.display = 'none';
+                NovaPoshtaToBranch.style.display = 'block';
+                worldwide.checked = false;
+                novpostodoor.checked = false;
+                urkpostobranch.checked = false;
+                urkpostodoor.checked = false;
+                justintobranch.checked = false;
+                } else {
+            NovaPoshtaToBranch.style.display = 'none';
             };
         });
         novpostodoor.addEventListener('click', () => {
+            WorldWide.style.display = 'none';
+            NovaPoshtaToBranch.style.display = 'none';
+            UkrposhtaToBranch.style.display = 'none';
+            JustinToBranch.style.display = 'none';
+            UkrposhtaToDoor.style.display = 'none';
+            NovaPoshtaToDoor.style.display = 'block';
             if (novpostodoor.checked) {
-                novpostobranch.checked = false
-                worldwide.checked = false
-                urkpostobranch.checked = false
-                urkpostodoor.checked = false
-                justintobranch.checked = false
+                WorldWide.style.display = 'none';
+                NovaPoshtaToBranch.style.display = 'none';
+                UkrposhtaToBranch.style.display = 'none';
+                JustinToBranch.style.display = 'none';
+                UkrposhtaToDoor.style.display = 'none';
+                NovaPoshtaToDoor.style.display = 'block';
+                novpostobranch.checked = false;
+                worldwide.checked = false;
+                urkpostobranch.checked = false;
+                urkpostodoor.checked = false;
+                justintobranch.checked = false;
+                } else {
+            NovaPoshtaToDoor.style.display = 'none';
             };
         });
         urkpostobranch.addEventListener('click', () => {
             if (urkpostobranch.checked) {
-                novpostobranch.checked = false
-                novpostodoor.checked = false
-                worldwide.checked = false
-                urkpostodoor.checked = false
-                justintobranch.checked = false
+                WorldWide.style.display = 'none';
+                NovaPoshtaToBranch.style.display = 'none';
+                NovaPoshtaToDoor.style.display = 'none';
+                JustinToBranch.style.display = 'none';
+                UkrposhtaToDoor.style.display = 'none';
+                UkrposhtaToBranch.style.display = 'block';
+                novpostobranch.checked = false;
+                novpostodoor.checked = false;
+                worldwide.checked = false;
+                urkpostodoor.checked = false;
+                justintobranch.checked = false;
+                } else {
+            UkrposhtaToBranch.style.display = 'none';
             };
         });
         urkpostodoor.addEventListener('click', () => {
             if (urkpostodoor.checked) {
-                novpostobranch.checked = false
-                novpostodoor.checked = false
-                urkpostobranch.checked = false
-                worldwide.checked = false
-                justintobranch.checked = false
+                WorldWide.style.display = 'none';
+                NovaPoshtaToBranch.style.display = 'none';
+                NovaPoshtaToDoor.style.display = 'none';
+                UkrposhtaToBranch.style.display = 'none';
+                JustinToBranch.style.display = 'none';
+                UkrposhtaToDoor.style.display = 'block';
+                novpostobranch.checked = false;
+                novpostodoor.checked = false;
+                urkpostobranch.checked = false;
+                worldwide.checked = false;
+                justintobranch.checked = false;
+                } else {
+            UkrposhtaToDoor.style.display = 'none';
             };
         });
         justintobranch.addEventListener('click', () => {
             if (justintobranch.checked) {
-                novpostobranch.checked = false
-                novpostodoor.checked = false
-                urkpostobranch.checked = false
-                urkpostodoor.checked = false
-                worldwide.checked = false
+                WorldWide.style.display = 'none';
+                NovaPoshtaToBranch.style.display = 'none';
+                NovaPoshtaToDoor.style.display = 'none';
+                UkrposhtaToBranch.style.display = 'none';
+                UkrposhtaToDoor.style.display = 'none';
+                JustinToBranch.style.display = 'block';
+                novpostobranch.checked = false;
+                novpostodoor.checked = false;
+                urkpostobranch.checked = false;
+                urkpostodoor.checked = false;
+                worldwide.checked = false;
+                } else {
+            JustinToBranch.style.display = 'none';
             };
         });
-    };
+   };
+
+    const checkout = document.getElementById("pop-order-checkout");
+    checkout.addEventListener('click', () => {
+        if (worldwide.checked) {
+            NovaPoshtaToBranch.parentElement.removeChild(NovaPoshtaToBranch);
+            NovaPoshtaToDoor.parentElement.removeChild(NovaPoshtaToDoor);
+            UkrposhtaToBranch.parentElement.removeChild(UkrposhtaToBranch);
+            UkrposhtaToDoor.parentElement.removeChild(UkrposhtaToDoor);
+            JustinToBranch.parentElement.removeChild(JustinToBranch);
+        };
+        if (novpostobranch.checked) {
+            WorldWide.parentElement.removeChild(WorldWide);
+            NovaPoshtaToDoor.parentElement.removeChild(NovaPoshtaToDoor);
+            UkrposhtaToBranch.parentElement.removeChild(UkrposhtaToBranch);
+            UkrposhtaToDoor.parentElement.removeChild(UkrposhtaToDoor);
+            JustinToBranch.parentElement.removeChild(JustinToBranch);
+        };
+        if (novpostodoor.checked) {
+            WorldWide.parentElement.removeChild(WorldWide);
+            NovaPoshtaToBranch.parentElement.removeChild(NovaPoshtaToBranch);
+            UkrposhtaToBranch.parentElement.removeChild(UkrposhtaToBranch);
+            UkrposhtaToDoor.parentElement.removeChild(UkrposhtaToDoor);
+            JustinToBranch.parentElement.removeChild(JustinToBranch);
+        };
+        if (urkpostobranch.checked) {
+            WorldWide.parentElement.removeChild(WorldWide);
+            NovaPoshtaToBranch.parentElement.removeChild(NovaPoshtaToBranch);
+            NovaPoshtaToDoor.parentElement.removeChild(NovaPoshtaToDoor);
+            UkrposhtaToDoor.parentElement.removeChild(UkrposhtaToDoor);
+            JustinToBranch.parentElement.removeChild(JustinToBranch);
+        };
+        if (urkpostodoor.checked) {
+            WorldWide.parentElement.removeChild(WorldWide);
+            NovaPoshtaToBranch.parentElement.removeChild(NovaPoshtaToBranch);
+            NovaPoshtaToDoor.parentElement.removeChild(NovaPoshtaToDoor);
+            UkrposhtaToBranch.parentElement.removeChild(UkrposhtaToBranch);
+            JustinToBranch.parentElement.removeChild(JustinToBranch);
+        };
+        if (justintobranch.checked) {
+            WorldWide.parentElement.removeChild(WorldWide);
+            NovaPoshtaToBranch.parentElement.removeChild(NovaPoshtaToBranch);
+            NovaPoshtaToDoor.parentElement.removeChild(NovaPoshtaToDoor);
+            UkrposhtaToBranch.parentElement.removeChild(UkrposhtaToBranch);
+            UkrposhtaToDoor.parentElement.removeChild(UkrposhtaToDoor);
+        };
+    });
+//-----------   MAKE ORDER MODAL WINDOW     -----------
 
 
     const toggle = document.querySelector('.toggle input');
@@ -121,8 +236,6 @@ $(document).ready(function() {
             document.getElementById("category-style").classList.add('collection-template__products')
             }
     });
-
-
 
 
 });
